ccf5ced447c3f794c33137a1ae7f2dff7ccc0dd4
Ubuntu <ubuntu@ip-172-31-1-68.us-east-2.compute.internal>
2023-04-24 02:15:16 +0000

    corrección pytest

--- test_randomsum.py
+++ test_randomsum.py
@@ -2,13 +2,13 @@ import subprocess
 
 def test_python_command_execution():
     # Ejecuta el comando de Python y obtiene el código de salida y la salida estándar
-    result = subprocess.run(["python3", "test_randomsum.py"], capture_output=True, text=True)
+    result = subprocess.run(["python3", "randomsum.py"], capture_output=True, text=True)
     exit_code = result.returncode
     stdout = result.stdout
 
     # Verifica que el código de salida sea 0
     assert exit_code == 0, f"El código de salida fue {exit_code}. Se esperaba 0."
 
-    # Verifica que tres líneas distintas tengan valores diferentes a nulo o cero
+    # Verifica que al menos 1 línea tenga valores diferentes a nulo o cero
     lines = stdout.split("\n")
-    assert len(lines) >= 1, "El código de salida fue diferente de 0"
+    assert len(lines) >= 1, "La salida de la app debe tener al menos 1 línea"
