2c1ffb37feb78a88fe43bc85053b860c84a505b8
Ubuntu <ubuntu@ip-172-31-1-68.us-east-2.compute.internal>
2023-04-24 02:10:36 +0000

    change test python

--- test_randomsum.py
+++ test_randomsum.py
@@ -1,8 +1,14 @@
-import random
+import subprocess
 
-num1 = random.randint(1,100)
-num2 = random.randint(1,100)
+def test_python_command_execution():
+    # Ejecuta el comando de Python y obtiene el código de salida y la salida estándar
+    result = subprocess.run(["python3", "test_randomsum.py"], capture_output=True, text=True)
+    exit_code = result.returncode
+    stdout = result.stdout
 
-suma = num1 + num2
+    # Verifica que el código de salida sea 0
+    assert exit_code == 0, f"El código de salida fue {exit_code}. Se esperaba 0."
 
-print("El resultado de sumar ", num1, "y", num2, "es:", suma)
+    # Verifica que tres líneas distintas tengan valores diferentes a nulo o cero
+    lines = stdout.split("\n")
+    assert len(lines) >= 1, "El código de salida fue diferente de 0"
